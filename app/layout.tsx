daccbffbc624afd2d92dbc2eb0d47345b005c9df
Michal Juhas <michal@michaljuhas.com>
2026-03-19 15:03:04 +0100

    Add Meta Pixel to <head>

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,6 +73,15 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     })`,
             }}
           />
+          {/* Meta Pixel */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1227582702838805');fbq('track','PageView');`,
+            }}
+          />
+          <noscript>
+            <img height="1" width="1" style={{ display: "none" }} src="https://www.facebook.com/tr?id=1227582702838805&ev=PageView&noscript=1" alt="" />
+          </noscript>
         </head>
         <body className="min-h-full antialiased">
           {GTM_ID && (
